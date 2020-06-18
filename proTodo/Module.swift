3eadcca6bc229e4c6c484e61ae1a4f385d640b75
artificIn <djgmd1021@gmail.com>
2020-06-18 23:32:26 +0900

    revise data mpdel

--- proTodo/Module.swift
+++ proTodo/Module.swift
@@ -8,83 +8,54 @@
 
 import Foundation
 
-let projectName = "ProTodo"
+let ProTodoName = "ProTodo"
+let TodoListFileName = "TodoList.file"
+let ProjectListFileName = "ProjectList.file"
+let LabelListFileName = "LabelList.file"
 
-let TodoDatabase : TodoModel = TodoModel()
-let TodoFileName = "ProTodoList.file"
 
+protocol Codability : Codable {}
 
-
-enum Colors {
-    case 0 :
+extension Codability {
+    typealias T = Self
+    func encode() -> Data? {
+        return try? JSONEncoder().encode(self)
+    }
+    
+    static func decode(data : Data) -> T? {
+        return try? JSONDecoder().decode(T.self, from: data)
+    }
 }
 
-struct Labelling {
+
+struct Label : Codability {
     var name : String
     var color : Int
 }
 
-struct Project {
+struct LabelList : Codability {
+    var arrayList : [Label]
+}
+
+
+struct Todo : Codability {
+    var title : String
+    var label : [Label]
+    var date : String
+}
+
+struct TodoList : Codability {
+    var arrayList : [Todo]
+}
+
+
+struct Project : Codability {
     var name : String
-    var endDate : Date
+    var date : String
+    var notice : Bool
+    var todo : [Todo]
 }
 
-class Todo : NSObject, NSCoding {
-    var memo : String
-    var color : Int
-    var update : Date // 소속 날짜
-    var labels : [Labelling]?
-    var projects : [Project]?
-    
-    init(memo:String, color:Int, update: Date) {
-        self.memo = memo
-        self.color = color
-        self.update = update
-    }
-    
-    func encode(with coder: NSCoder) {
-        coder.encode(self.memo, forKey: "memo")
-        coder.encode(self.color, forKey: "color")
-        coder.encode(self.update, forKey: "update")
-    }
-    
-    required init?(coder: NSCoder) {
-        self.memo = coder.decodeObject(forKey: "memo") as! String
-        self.color = coder.decodeObject(forKey: "color") as! Int
-        self.update = coder.decodeObject(forKey: "update") as! Date
-    }
+struct ProjectList : Codability {
+    var arrayList : [Project]
 }
-
-
-
-class TodoModel {
-    var arrayList : [Todo] = [] // 일반 저장
-    var repeatList : [Todo] = [] // 반복하는 할일 저장
-    
-    var mainfilePath: String { get {
-        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
-        return documentDirectory + "/" + projectName + "/" + TodoFileName
-        }}
-    
-    func saveData(){
-        NSKeyedArchiver.archiveRootObject(self.arrayList, toFile: self.mainfilePath)
-    }
-    
-    func defaultData() -> Array<Todo>{
-        let stock = Todo(memo: "앱 업데이트", color: 0xafeeee, update: Date())
-        let stock2 = Todo(memo: "쇼핑하기", color: 0xffbe46, update: Date() - 86400)
-        let stock3 = Todo(memo: "운동하기", color: 0x34de53, update: Date())
-        return [stock, stock2, stock3]
-    }
-    
-    init() {
-        if FileManager.default.fileExists(atPath: self.mainfilePath){
-            if let mainArray = NSKeyedUnarchiver.unarchiveObject(withFile: self.mainfilePath) as? [Todo] {
-                arrayList = mainArray
-            }
-        } else {
-            arrayList = defaultData()
-        }
-    }
-}
-
